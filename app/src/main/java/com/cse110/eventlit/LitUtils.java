dd77b2d098bf2ff1805e6718578d4e0e106c4a86
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-02-15 02:32:07 -0800

    Added Utils class with a static method to hide the soft keyboard

---
+++ app/src/main/java/com/cse110/eventlit/LitUtils.java
@@ -0,0 +1,20 @@
+package com.cse110.eventlit;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by rahulsabnis on 2/15/17.
+ */
+
+public class LitUtils {
+
+    public static void hideSoftKeyboard (AppCompatActivity activity, View view)
+    {
+        InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
+    }
+
+}
