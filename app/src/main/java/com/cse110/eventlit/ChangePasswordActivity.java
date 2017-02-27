2b9fa6db2b01e0d9976e2a3c3c3b91e2b579ba32
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-02-27 00:02:19 -0800

    Integrated Backend Password Change Feature
    -You may now change your password from the UI

--- app/src/main/java/com/cse110/eventlit/ChangePasswordActivity.java
+++ app/src/main/java/com/cse110/eventlit/ChangePasswordActivity.java
@@ -1,12 +1,18 @@
 package com.cse110.eventlit;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.cse110.utils.UserUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Michelle on 2/23/2017.
@@ -18,6 +24,7 @@ public class ChangePasswordActivity  extends AppCompatActivity {
     private EditText mOldPass;
     private EditText mPass1;
     private EditText mPass2;
+    private FirebaseUser mUser;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -30,32 +37,50 @@ public class ChangePasswordActivity  extends AppCompatActivity {
             mPass1 = (EditText)findViewById(R.id.newPassword1);
             mPass2 = (EditText)findViewById(R.id.newPassword2);
 
+            // Get the Firebase user instance
+            mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
 
             // Handling Change Password Button Listener
             mSubmitChangePass.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     // Get TextValues
-                    String oldPass = mOldPass.getText().toString();
-                    String newPass1 = mPass1.getText().toString();
-                    String newPass2 = mPass2.getText().toString();
+                    String oldPass = mOldPass.getText() + "";
+                    String newPass1 = mPass1.getText() + "";
+                    String newPass2 = mPass2.getText() + "";
 
-                    // Edge Case: Empty params
-                    if (oldPass == null || newPass1 == null || newPass2 == null) {
+                    // Do this when reset is done
+                    OnCompleteListener<String> passwordResetListener = new OnCompleteListener<String>() {
+                        @Override
+                        public void onComplete(@NonNull Task<String> task) {
+                            Toast result = Toast.makeText(ChangePasswordActivity.this,
+                                    task.getResult(), Toast.LENGTH_SHORT);
+                            result.show();
+                        }
+                    };
 
-
+                    // Checks for empty input
+                    if (oldPass.equals("") || newPass1.equals("") || newPass2.equals("")) {
+                        Toast toast = Toast.makeText(ChangePasswordActivity.this,
+                                "Please enter valid password", Toast.LENGTH_LONG);
+                        toast.show();
                     }
 
                     // Edge Case: new passwords do not match
-                    if (newPass1 != newPass2){
+                    else if (!newPass1.equals(newPass2)){
+                        Toast error = Toast.makeText(ChangePasswordActivity.this,
+                                "Passwords do not match", Toast.LENGTH_SHORT);
+                        error.show();
+                    }
+                    else {
+                        // PS: Backend handles case for incorrect old password
 
+                        // Backend will take care of it from here
+                        UserUtils.resetPassword(mUser, oldPass, newPass1, passwordResetListener);
                     }
 
-                    // Edge Case: Old password is incorrect
-                    // TODO if (oldPass != )
-
-
-                    // TODO Change Password
                 }
             });
 
