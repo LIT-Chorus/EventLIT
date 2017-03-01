e8bc63ed79f320acfef05d5de32cc37598eb917a
Vansh <vanshchaudhary@gmail.com>
2017-03-01 00:53:04 -0800

    added FileStorageUtils class and test

---
+++ app/src/androidTest/java/com/cse110/eventlit/FileStorageUtilsTest.java
@@ -0,0 +1,90 @@
+package com.cse110.eventlit;
+
+/**
+ * Created by vansh on 2/28/17.
+ */
+
+import android.support.annotation.NonNull;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.cse110.eventlit.db.User;
+import com.cse110.utils.FileStorageUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+@RunWith(AndroidJUnit4.class)
+public class FileStorageUtilsTest {
+
+    private static final String USER_EMAIL = "vcsingh@ucsd.edu";
+    private static final String USER_PASSWORD = "heythere";
+
+    FirebaseAuth fbAuth;
+    DatabaseReference fbDB;
+
+    String uid;
+
+    @Before
+    public void setUp() throws Exception {
+        fbAuth = FirebaseAuth.getInstance();
+        fbDB = FirebaseDatabase.getInstance().getReference();
+
+        //final String[] uid = new String[1];
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        fbAuth.signInWithEmailAndPassword(USER_EMAIL, USER_PASSWORD).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser fbUser = fbAuth.getCurrentUser();
+                    uid = fbUser.getUid();
+
+                } else {
+                    Log.e("UserUtilsTest", "Authorization task not successful!");
+                }
+            }
+        });
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.wtf("UserUtilsTest", "setup is complete");
+    }
+
+    @Test
+    void addPictureToUser() {
+        FileStorageUtils.uploadImageFromLocalFile(uid, "~/AndroidStudioProjects/events-android/src/main/res/drawable/app_icon.png");
+    }
+
+    @Test
+    void getPictureFromUser() {
+
+    }
+
+    @Test
+    void addPictureToEvent() {
+
+    }
+
+    @Test
+    void getPictureFromEvent() {
+
+    }
+}
