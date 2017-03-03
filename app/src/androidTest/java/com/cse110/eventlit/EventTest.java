a64a776ba36f89481cc5a450c4a96f572b9d79d9
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-03-03 00:48:49 -0800

    Added CreateEvent utils method
    -In EventUtils. To be called with an onCompleteListener and
    an Event object.

--- app/src/androidTest/java/com/cse110/eventlit/EventTest.java
+++ app/src/androidTest/java/com/cse110/eventlit/EventTest.java
@@ -1,9 +1,13 @@
 package com.cse110.eventlit;
 
+import android.support.annotation.NonNull;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import com.cse110.eventlit.db.Event;
+import com.cse110.utils.EventUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,6 +21,7 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -80,5 +85,27 @@ public class EventTest {
     public void testFormat() throws Exception {
         String fmt = "yyyy-MM-dd";
         Log.d("testFormat", event.formattedStartTime(fmt));
+    }
+
+    @Test
+    public void testCreatedEvent() throws  Exception {
+        Event createdEvent = new Event("Test Create Event", "Here it is.", "test-org-1",
+                now, now, "Nowhere", "Other", 100);
+        final CountDownLatch latch = new CountDownLatch(1);
+        OnCompleteListener onCompleteListener = new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                assertEquals(true, task.isSuccessful());
+                latch.countDown();
+            }
+        };
+        EventUtils.createEvent(createdEvent, onCompleteListener);
+        try {
+            latch.await();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
