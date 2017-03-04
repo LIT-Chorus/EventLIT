a65e98512b178ce24eeb1c0fe58a1cfa31a862fc
Michelle Yang <zomgitsmichelle@gmail.com>
2017-03-03 17:31:48 -0800

    Modified spacing of settings page.

--- app/src/main/java/com/cse110/eventlit/SettingsActivity.java
+++ app/src/main/java/com/cse110/eventlit/SettingsActivity.java
@@ -29,6 +29,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -59,93 +60,12 @@ import java.util.List;
 public class SettingsActivity extends AppCompatActivity implements IPickResult
 {
 
-    private ImageButton mChangePass;
-    private ImageButton mReqOrgStatus;
+    private LinearLayout mChangePass;
+    private LinearLayout mReqOrgStatus;
     private de.hdodenhof.circleimageview.CircleImageView mProfilePhoto;
     private TextView mName;
 
 
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-//    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-//        @Override
-//        public boolean onPreferenceChange(Preference preference, Object value) {
-//            String stringValue = value.toString();
-//
-//            if (preference instanceof ListPreference) {
-//                // For list preferences, look up the correct display value in
-//                // the preference's 'entries' list.
-//                ListPreference listPreference = (ListPreference) preference;
-//                int index = listPreference.findIndexOfValue(stringValue);
-//
-//                // Set the summary to reflect the new value.
-//                preference.setSummary(
-//                        index >= 0
-//                                ? listPreference.getEntries()[index]
-//                                : null);
-//
-//            } else if (preference instanceof RingtonePreference) {
-//                // For ringtone preferences, look up the correct display value
-//                // using RingtoneManager.
-//                if (TextUtils.isEmpty(stringValue)) {
-//                    // Empty values correspond to 'silent' (no ringtone).
-//                    preference.setSummary(R.string.pref_ringtone_silent);
-//
-//                } else {
-//                    Ringtone ringtone = RingtoneManager.getRingtone(
-//                            preference.getContext(), Uri.parse(stringValue));
-//
-//                    if (ringtone == null) {
-//                        // Clear the summary if there was a lookup error.
-//                        preference.setSummary(null);
-//                    } else {
-//                        // Set the summary to reflect the new ringtone display
-//                        // name.
-//                        String name = ringtone.getTitle(preference.getContext());
-//                        preference.setSummary(name);
-//                    }
-//                }
-//
-//            } else {
-//                // For all other preferences, set the summary to the value's
-//                // simple string representation.
-//                preference.setSummary(stringValue);
-//            }
-//            return true;
-//        }
-//    };
-//
-//    /**
-//     * Helper method to determine if the device has an extra-large screen. For
-//     * example, 10" tablets are extra-large.
-//     */
-//    private static boolean isXLargeTablet(Context context) {
-//        return (context.getResources().getConfiguration().screenLayout
-//                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-//    }
-//
-//    /**
-//     * Binds a preference's summary to its value. More specifically, when the
-//     * preference's value is changed, its summary (line of text below the
-//     * preference title) is updated to reflect the value. The summary is also
-//     * immediately updated upon calling this method. The exact display format is
-//     * dependent on the type of preference.
-//     *
-//     * @see #sBindPreferenceSummaryToValueListener
-//     */
-//    private static void bindPreferenceSummaryToValue(Preference preference) {
-//        // Set the listener to watch for value changes.
-//        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-//
-//        // Trigger the listener immediately with the preference's
-//        // current value.
-//        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-//                PreferenceManager
-//                        .getDefaultSharedPreferences(preference.getContext())
-//                        .getString(preference.getKey(), ""));
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -174,8 +94,8 @@ public class SettingsActivity extends AppCompatActivity implements IPickResult
 
 
         // Initialize buttons
-        mChangePass = (ImageButton) findViewById(R.id.changePass);
-        mReqOrgStatus = (ImageButton) findViewById(R.id.reqOrgStatus);
+        mChangePass = (LinearLayout) findViewById(R.id.changePass);
+        mReqOrgStatus = (LinearLayout) findViewById(R.id.reqOrgStatus);
 
         // Handling Change Password Button Listener
         mChangePass.setOnClickListener(new View.OnClickListener() {
