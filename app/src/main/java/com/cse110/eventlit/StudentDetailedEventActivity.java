7dd0b81cacc849bafcba343a2a6892ef2418c629
Michelle Yang <zomgitsmichelle@gmail.com>
2017-03-06 14:16:10 -0800

    Fixing merge conflicts.

--- app/src/main/java/com/cse110/eventlit/StudentDetailedEventActivity.java
+++ app/src/main/java/com/cse110/eventlit/StudentDetailedEventActivity.java
@@ -1,10 +1,16 @@
 package com.cse110.eventlit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import me.grantland.widget.AutofitHelper;
 
 public class StudentDetailedEventActivity extends AppCompatActivity {
 
@@ -14,6 +20,11 @@ public class StudentDetailedEventActivity extends AppCompatActivity {
         setContentView(R.layout.student_activity_detailed_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Fill in the detailed events info with bundle passed in
+        Intent i = getIntent();
+        Bundle extras = i.getExtras();
+        fillFields(extras);
 
         // TODO: Set database going/interested/not going entry
         Button goingBut = (Button) findViewById(R.id.goingButton);
@@ -47,6 +58,25 @@ public class StudentDetailedEventActivity extends AppCompatActivity {
     public void onBackPressed() {
         // TODO: Go back to organizer feed
         finish();
+
+    }
+
+    /* Sets the text fields dynamically */
+    private void fillFields(Bundle bundle) {
+
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText(bundle.getString("eventName"));
+        AutofitHelper.create(title);
+
+        TextView date = (TextView) findViewById(R.id.timetext);
+        date.setText(bundle.getString("date").replaceAll("[\\t\\n\\r]+"," ")
+                + " at " + bundle.getString("time").trim());
+
+        TextView location = (TextView) findViewById(R.id.locationtext);
+        location.setText(bundle.getString("location"));
+
+        TextView category = (TextView) findViewById(R.id.tagtext);
+        category.setText(bundle.getString("category"));
 
     }
 
