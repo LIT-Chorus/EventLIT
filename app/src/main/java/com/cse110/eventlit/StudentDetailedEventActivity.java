8e0cc3f58d894a17e5a26c04a269ba1b97a59a38
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-03-11 06:49:34 -0800

    RSVP Status Updates Visually On all Pages and Pulls Status from DB

--- app/src/main/java/com/cse110/eventlit/StudentDetailedEventActivity.java
+++ app/src/main/java/com/cse110/eventlit/StudentDetailedEventActivity.java
@@ -1,18 +1,27 @@
 package com.cse110.eventlit;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cse110.eventlit.db.RSVP;
+import com.cse110.utils.UserUtils;
 
 import org.w3c.dom.Text;
 
 import me.grantland.widget.AutofitHelper;
 
 public class StudentDetailedEventActivity extends AppCompatActivity {
+
+    private String orgId;
+    private String eventId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,29 +36,56 @@ public class StudentDetailedEventActivity extends AppCompatActivity {
         fillFields(extras);
 
         // TODO: Set database going/interested/not going entry
-        Button goingBut = (Button) findViewById(R.id.goingButton);
-        Button interestedBut = (Button) findViewById(R.id.interestedButton);
-        Button notGoingBut = (Button) findViewById(R.id.notGoingButton);
+        final Button goingBut = (Button) findViewById(R.id.goingButton);
+        final Button interestedBut = (Button) findViewById(R.id.interestedButton);
+        final Button notGoingBut = (Button) findViewById(R.id.notGoingButton);
+
+        int rsvp = extras.getInt("RSVP");
+        if (rsvp == 1) {
+            goingBut.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.goingColor, null));
+            interestedBut.setBackgroundColor(Color.GRAY);
+            notGoingBut.setBackgroundColor(Color.GRAY);
+        } else if (rsvp == 2) {
+            interestedBut.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.interestedColor, null));
+            goingBut.setBackgroundColor(Color.GRAY);
+            notGoingBut.setBackgroundColor(Color.GRAY);
+        } else if (rsvp == 3) {
+            notGoingBut.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.notGoingColor, null));
+            goingBut.setBackgroundColor(Color.GRAY);
+            interestedBut.setBackgroundColor(Color.GRAY);
+        }
 
 
         goingBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                RSVP status = new RSVP(orgId, eventId, RSVP.Status.GOING);
+                UserUtils.addEventsFollowing(eventId, status);
+                goingBut.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.goingColor, null));
+                interestedBut.setBackgroundColor(Color.GRAY);
+                notGoingBut.setBackgroundColor(Color.GRAY);
             }
         });
 
         interestedBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                RSVP status = new RSVP(orgId, eventId, RSVP.Status.INTERESTED);
+                UserUtils.addEventsFollowing(eventId, status);
+                interestedBut.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.interestedColor, null));
+                goingBut.setBackgroundColor(Color.GRAY);
+                notGoingBut.setBackgroundColor(Color.GRAY);
             }
         });
 
         notGoingBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                RSVP status = new RSVP(orgId, eventId, RSVP.Status.NOT_GOING);
+                UserUtils.addEventsFollowing(eventId, status);
+                notGoingBut.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.notGoingColor, null));
+                goingBut.setBackgroundColor(Color.GRAY);
+                interestedBut.setBackgroundColor(Color.GRAY);
             }
         });
     }
@@ -57,8 +93,8 @@ public class StudentDetailedEventActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         // TODO: Go back to organizer feed
+        startActivity(new Intent(this, StudentFeedActivity.class));
         finish();
-
     }
 
     /* Sets the text fields dynamically */
@@ -77,6 +113,18 @@ public class StudentDetailedEventActivity extends AppCompatActivity {
 
         TextView category = (TextView) findViewById(R.id.tagtext);
         category.setText(bundle.getString("category"));
+
+        TextView description = (TextView) findViewById(R.id.descriptiontext);
+        description.setText(bundle.getString("description"));
+
+        TextView numAttend = (TextView) findViewById(R.id.peopletext);
+        numAttend.setText("" + bundle.getInt("num_attending"));
+
+        TextView name = (TextView) findViewById(R.id.orgname);
+        name.setText("Hosted By " + bundle.getString("org_name"));
+
+        orgId = bundle.getString("org_id");
+        eventId = bundle.getString("event_id");
 
     }
 
