a1975d5c9488c011d22b17b386a63d37909803cc
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-03-11 02:28:03 -0800

    Add Logic to verify the input matches on of the options in the Organizer Request Page

--- app/src/main/java/com/cse110/utils/LitUtils.java
+++ app/src/main/java/com/cse110/utils/LitUtils.java
@@ -1,9 +1,16 @@
 package com.cse110.utils;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+
+import com.cse110.eventlit.db.Organization;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
 
 /**
  * Created by rahulsabnis on 2/15/17.
