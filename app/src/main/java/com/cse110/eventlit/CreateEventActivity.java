3f90425404f469ba8dcbc345daa710b70e4f87ee
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-03-11 04:08:39 -0800

    Create Event Now Shows Organization Names as Names instead of IDs

--- app/src/main/java/com/cse110/eventlit/CreateEventActivity.java
+++ app/src/main/java/com/cse110/eventlit/CreateEventActivity.java
@@ -66,7 +66,11 @@ public class CreateEventActivity extends AppCompatActivity {
 
         // Gets the Orgs that the Organization User is managing
         User user = UserUtils.getCurrentUser();
-        orgsManaging = user.getOrgsManaging(); 
+        ArrayList<Organization> orgs = UserUtils.getOrgsManaging();
+        orgsManaging = new ArrayList<>();
+        for (Organization org: orgs) {
+            orgsManaging.add(org.getName());
+        }
 
         // Cancel and Create button functionality
         Button cancelBut = (Button) findViewById(R.id.cancelButton);
@@ -91,13 +95,11 @@ public class CreateEventActivity extends AppCompatActivity {
 
         // Populate spinner for selecting org that the event is for
         Spinner spinner = (Spinner)findViewById(R.id.spinner);
-     /*   spinner.setPrompt("Organization holding event");
+        spinner.setPrompt("Organization holding event");
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, android.R.id.text1);
+                android.R.layout.simple_spinner_item, android.R.id.text1, orgsManaging);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
-        spinnerAdapter.add("value");
-        spinnerAdapter.notifyDataSetChanged();*/
 
         // Input Fields
         mTitle = (TextInputLayout) findViewById(R.id.title);
