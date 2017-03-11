53214ccb0c476690c97ffee5efaa569b131500cd
Jocelyn Tran <jocelyn.tran1@gmail.com>
2017-03-11 03:01:13 -0800

    Add Date Picker Fragment for Create Event Page

---
+++ app/src/main/java/com/cse110/eventlit/DatePickerFragment.java
@@ -0,0 +1,41 @@
+package com.cse110.eventlit;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by jocelyn on 3/11/17.
+ */
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        //Use the current date as the default date in the date picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        //Create a new DatePickerDialog instance and return it
+        /*
+            DatePickerDialog Public Constructors - Here we uses first one
+            public DatePickerDialog (Context context, DatePickerDialog.OnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth)
+            public DatePickerDialog (Context context, int theme, DatePickerDialog.OnDateSetListener listener, int year, int monthOfYear, int dayOfMonth)
+         */
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        //Do something with the date chosen by the user
+        TextView tv = (TextView) getActivity().findViewById(getArguments().getInt("datetext"));
+
+        String stringOfDate = month + "/" + day + "/" + year;
+        tv.setText(tv.getText() + stringOfDate);
+    }
+}
