daac7bf08862a43cb3232aa527e1b79b2b257d4a
Jocelyn Tran <jocelyn.tran1@gmail.com>
2017-03-18 20:48:31 -0700

    Add util methods to convert unix timestamp to strings to display

--- app/src/main/java/com/cse110/utils/LitUtils.java
+++ app/src/main/java/com/cse110/utils/LitUtils.java
@@ -10,7 +10,10 @@ import com.cse110.eventlit.db.Organization;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by rahulsabnis on 2/15/17.
@@ -23,5 +26,28 @@ public class LitUtils {
         InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
     }
+
+    public static String getMonth(long unixSeconds) {
+        Date date = new Date(unixSeconds*1000L); // *1000 is to convert seconds to milliseconds
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-3M-dd HH:mm a"); // the format of the date
+        String formattedDate = sdf.format(date);
+        return formattedDate.substring(5, 8);
+    }
+
+    public static String getDay(long unixSeconds) {
+        Date date = new Date(unixSeconds*1000L); // *1000 is to convert seconds to milliseconds
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-3M-dd HH:mm a"); // the format of the date
+        String formattedDate = sdf.format(date);
+        return formattedDate.substring(10, 12);
+    }
+
+    public static String getTime(long unixSeconds) {
+        Date date = new Date(unixSeconds*1000L); // *1000 is to convert seconds to milliseconds
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-3M-dd HH:mm a"); // the format of the date
+        String formattedDate = sdf.format(date);
+        return formattedDate.substring(12, formattedDate.length() - 1);
+    }
+
+
 
 }
